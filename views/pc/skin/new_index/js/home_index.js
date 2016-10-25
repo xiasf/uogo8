4c1f530c3f094681797c2976e1319cc7087b62e6
xiasf <811800545@qq.com>
2016-10-26 04:17:15 +0800

    很多细节修复，完善

--- views/pc/skin/new_index/js/home_index.js
+++ views/pc/skin/new_index/js/home_index.js
@@ -319,7 +319,7 @@ $(function(){
 	setTimeout("takeCount()", 1000);
     //首页Tab标签卡滑门切换
     $(".tabs-nav > li > h3").bind('mouseover', (function(e) {
-    	if (e.target == this) {
+    	// if (e.target == this) {
     		var tabs = $(this).parent().parent().children("li");
     		var panels = $(this).parent().parent().parent().children(".tabs-panel");
     		var index = $.inArray(this, $(this).parent().parent().find("h3"));
@@ -327,7 +327,7 @@ $(function(){
     			tabs.removeClass("tabs-selected").eq(index).addClass("tabs-selected");
     			panels.addClass("tabs-hide").eq(index).removeClass("tabs-hide");
     		}
-    	}
+    	// }
     }));
 
 	$('.jfocus-trigeminy > ul > li > a').jfade({
