cbfa0f7cf43dc8065422f2e316af3799c1c0dea4
xiasf <811800545@qq.com>
2016-10-27 18:49:33 +0800

    修复首页细节...

--- views/pc/javascript/site.js
+++ views/pc/javascript/site.js
@@ -209,7 +209,7 @@ jQuery(function()
 var isIE = !-[1,];
 if (window["console"]) {
 	if ($.isFunction(console.log)) {
-			console.log("%c \u6dd8\u9ec4\u5dde\u5b89\u5168\u8b66\u544a", "font-size:50px;color:red;-webkit-text-fill-color:red;-webkit-text-stroke: 1px black;");
+			console.log("%c \u4f18\u8d2d\u7f51\u5b89\u5168\u8b66\u544a", "font-size:50px;color:#4C88E0;-webkit-text-fill-color:#000;-webkit-text-stroke: 1px black;");
 			console.log("%c \u6b64\u6d4f\u89c8\u5668\u529f\u80fd\u4e13\u4f9b\u5f00\u53d1\u8005\u4f7f\u7528\u3002\u8bf7\u4e0d\u8981\u5728\u6b64\u7c98\u8d34\u6267\u884c\u4efb\u4f55\u5185\u5bb9\uff0c\u8fd9\u53ef\u80fd\u4f1a\u5bfc\u81f4\u60a8\u7684\u8d26\u6237\u53d7\u5230\u653b\u51fb\uff0c\u7ed9\u60a8\u5e26\u6765\u635f\u5931 \uff01", "font-size: 20px;color:#333")
 	}
 }
